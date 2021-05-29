bcee44f3080f84943b936f0e1bbd33e2d4b6d71a
farOverNinethousand <36418572+farOverNinethousand@users.noreply.github.com>
2021-05-29 17:24:11 +0200

    Fixed NoDataAlarm

--- AlarmSystem.py
+++ AlarmSystem.py
@@ -182,7 +182,7 @@ class AlarmSystem:
                 infoText += " | Time until alarm: " + getFormattedDuration(self.noDataAlarmIntervalSeconds - durationNoNewData)
             logging.info(infoText)
             # Check if our alarm system maybe hasn't been responding for a long amount of time. Only send alarm for this once until data is back!
-            if -1 < self.noDataAlarmIntervalSeconds < datetime.now().timestamp() - self.lastEntryIDChangeTimestamp:
+            if -1 < self.noDataAlarmIntervalSeconds < datetime.now().timestamp() - self.lastSensorUpdateServersideDatetime.timestamp():
                 lastSensorDataIsFromDate = formatDatetimeToGermanDate(self.lastSensorUpdateServersideDatetime)
                 logging.warning("Got no new sensor data for a long time! Last data is from: " + lastSensorDataIsFromDate)
                 if not self.noDataAlarmHasBeenTriggered:
